8b2360953bf3b8097d159a3c3fc73c6b6e11cf1c
niedch <christoph.niederer99@gmail.com>
2020-05-21 16:54:24 +0200

    Change default waitress port

--- app/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hey, this is the Hello World endpoint'
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
